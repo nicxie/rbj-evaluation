f1deb7119996734eea609e74a313ec9e8f3c924b
Nica <nicagabs7@gmail.com>
2023-08-14 10:51:46 +0800

    Second Commit

--- resources/views/components/items.blade.php
+++ resources/views/components/items.blade.php
@@ -1,9 +1,7 @@
 <ul class="flex flex-col md:flex-row px-2 bg-gray-400">
+    @auth
     <li>
-        <a href="/login" class="block py-2 pr-4 pl-3 hover:text-blue-700">Sign In</a>
-    </li>
-    <li>
-        <a href="/register" class="block py-2 pr-4 pl-3 hover:text-blue-700">Sign Up</a>
+        <a href="/add/student" class="block py-2 pr-4 pl-3 hover:text-blue-700">Add New</a>
     </li>
     <li>
         <form action="/logout" method="POST">
@@ -11,4 +9,13 @@
             <button href="/logout" class="block py-2 pr-4 pl-3 hover:text-red-700">Logout</button>
         </form>
     </li>
+    @else
+
+    <li>
+        <a href="/login" class="block py-2 pr-4 pl-3 hover:text-blue-700">Sign In</a>
+    </li>
+    <li>
+        <a href="/register" class="block py-2 pr-4 pl-3 hover:text-blue-700">Sign Up</a>
+    </li>
+    @endauth
 </ul>
